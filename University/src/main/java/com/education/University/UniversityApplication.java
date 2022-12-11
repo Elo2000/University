6f8553676c80e4b79877728d98b7eb322b0c2ebd
Helena Khalilieh <96687936+Elo2000@users.noreply.github.com>
2022-12-11 13:54:55 +0200

    TASK2 commit (#2)

--- University/src/main/java/com/education/University/UniversityApplication.java
+++ University/src/main/java/com/education/University/UniversityApplication.java
@@ -1,13 +1,16 @@
 package com.education.University;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class UniversityApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(UniversityApplication.class, args);
+
+
 	}
 
 }
